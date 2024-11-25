0630aedafe05c527b9cb84bad04f3f7b2b4b86a7
Juliana Bonardi <julicbonardi@hotmail.com>
2024-11-24 23:13:25 -0300

    All functions working

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+// Lib for malloc, size_t and read
+# include <stdlib.h>
+# include <unistd.h>
+
+// Lib for Open
+// #include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
+
+// Utils functions
+size_t	ft_strlen(const char *s);
+char	  *ft_strdup(const char *s);
+char  	*ft_strchr(const char *s, int c);
+char	  *ft_strjoin(char const *s1, char const *s2);
+char	  *ft_substr(char const *s, unsigned int start, size_t len);
+void  	ft_bzero(void *s, size_t n);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,117 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	int		str_len;
+	char	*dup;
+
+	i = 0;
+	str_len = ft_strlen(s);
+	dup = (char *)malloc((str_len + 1) * sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int				i;
+	unsigned char	ch;
+
+	ch = (unsigned char)c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == ch)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (ch == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)s)[i] = 0;
+		i++;
+	}
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char		*str;
+	size_t		len;
+	size_t		i;
+	size_t		j;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < ft_strlen(s2))
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*substr;
+	unsigned int	i;
+	size_t			s_len;
+	size_t			subs_len;
+
+	if (s == NULL)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (s_len - start < len)
+		subs_len = s_len - start;
+	else
+		subs_len = len;
+	substr = (char *)malloc((subs_len + 1) * sizeof(char));
+	if (substr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < subs_len)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
