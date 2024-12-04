7e76f9684e894f27826d79b770182f4ee6f82381
Juliana Bonardi <julicbonardi@hotmail.com>
2024-12-03 23:02:00 -0300

    Taking off bzero

--- get_next_line.h
+++ get_next_line.h
@@ -20,6 +20,5 @@ char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	ft_bzero(void *s, size_t n);
 
 #endif
